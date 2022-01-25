3e51815917ac1d83bdd0bf06234a2c43c5c042b7
Qwarkeh <christine.f.stark@gmail.com>
2022-01-25 14:51:38 -0500

    passed 1st test, started planning for 2nd test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, manyMoreArgs) {
+
+//find length of array and manyMoreArgs
+//compare lengths
+//larger length ends up in the for loop
+//nest another for loop that loops through manyMoreArgs and compares it to array
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+
+    return array;
 
 };
 
