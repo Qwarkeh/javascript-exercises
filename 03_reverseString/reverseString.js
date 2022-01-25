b5adf4599784b251b02c3bb109bfd4570665d587
Qwarkeh <christine.f.stark@gmail.com>
2022-01-25 13:33:17 -0500

    completed exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
 const reverseString = function(str) {
+    let inputArray = [];
+    let outputArray = [];
 
+    // fill inputArray
+    for(let i = 0; i < str.length; i++) {
+        inputArray[i] = str[i];
+    }
+
+    //pop the end off the first array, and add it to the second array
+    for(let i = 0; i < str.length; i++) {
+        outputArray[i] = inputArray.pop();
+    }
+
+    outputString = outputArray.join('');
+    return outputString;
 };
 
 // Do not edit below this line
