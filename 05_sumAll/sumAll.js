241f50f7071f713b367ba0190607217d6879bb92
Qwarkeh <christine.f.stark@gmail.com>
2022-01-26 13:40:42 -0500

    passes first three tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+
+    const inputArray = [intOne, intTwo];
+    const sortedArray = inputArray.sort();
+
+    //create a new array and fill it
+    let incrementedArray = [sortedArray[0]];
+    let num = sortedArray[0];
+    for (let i = 1; i < sortedArray[1]; i++) {
+        num += 1;
+        incrementedArray.push(num);
+    }
+
+    //get the sum using reduce method
+    let sum = incrementedArray.reduce(function(a, b) {
+        return a + b;
+    }, 0);  
+
+    return sum;
 
 };
 
