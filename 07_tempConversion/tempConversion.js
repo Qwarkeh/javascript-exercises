4053842d7f8911172066d8f6d1c7d8ca22542972
Qwarkeh <christine.f.stark@gmail.com>
2022-01-26 14:41:44 -0500

    passes all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  celcius = parseFloat((( fahrenheit - 32) * (5/9)).toFixed(1));
+  return celcius;
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  fahrenheit = parseFloat((celcius * (9/5) + 32).toFixed(1))
+  return fahrenheit;
 };
 
 // Do not edit below this line
