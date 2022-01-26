f4e5d7e5f6c79418f82e5e4b5ab1437b52421c9c
Qwarkeh <christine.f.stark@gmail.com>
2022-01-26 16:35:42 -0500

    passed first 3 tests, failed 4th

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapYear;
+    if (year % 4 === 0) {
+        leapYear = true;
+    } else {
+        leapYear = false;
+    }
 
+    if (leapYear === true){
+        if (year % 100 === 0) {
+            if (year % 400 === 0){
+               leapYear = true;
+            }
+        }
+    }
+
+    return leapYear;
 };
 
 // Do not edit below this line
