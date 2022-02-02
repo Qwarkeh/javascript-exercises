f383ac073e8869468cd8cbf26e2ce221ca0bb621
Qwarkeh <christine.f.stark@gmail.com>
2022-02-02 11:13:24 -0500

    passes first five tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,20 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  const sum = num1 + num2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  const substraction = num1 - num2;
+  return substraction;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  else if (array.length === 1) {
+    return array[0];
+  }
 };
 
 const multiply = function() {
