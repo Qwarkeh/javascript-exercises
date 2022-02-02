7e9cb4882e5d3db854675bda478d310c8e55c2f2
Qwarkeh <christine.f.stark@gmail.com>
2022-02-02 13:25:40 -0500

    passes all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -16,12 +16,22 @@ const multiply = function(numbers) {
     return numbers.reduce((firstNumber, nextNumber) => firstNumber* nextNumber);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+    return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+  if (num === 0){
+    return 1;
+
+  } else {
+      let intArray = []; 
+      for (let i = 1; i < num + 1; i++) {
+      intArray.push(i);
+    } 
+    return intArray.reduce((firstNumber, nextNumber) => firstNumber * nextNumber);
+  }
 };
 
 // Do not edit below this line
