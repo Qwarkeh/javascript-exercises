3669f091e5f44f7c834563386fea5c769b56a0fb
Qwarkeh <christine.f.stark@gmail.com>
2022-02-02 15:56:34 -0500

    passes all six tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    
+    const inputArray = input.toLowerCase().split('');
 
+    const filteredArray= inputArray.filter(character => {
+        if (character !== '!' && character !== ',' && character !== ' ' && character !== '.')
+            return character;
+    });
+
+    let filteredClone = [...filteredArray]; //ES6 way of copying array
+
+    let reverseArray = filteredClone.reverse(); //reverse() changes original array
+
+    let inputNewString = filteredArray.join('');
+    let reverseArrayString = reverseArray.join('');
+
+    if (inputNewString === reverseArrayString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
