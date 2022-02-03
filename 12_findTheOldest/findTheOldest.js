ad0cfc5bc28f2a95c5da25b90ff66a2a0ec0f87c
Qwarkeh <christine.f.stark@gmail.com>
2022-02-03 11:15:39 -0500

    passes all tests, does not use .reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    let ageArray = [];
+    for (person of people) {
+        let name = person.name;
 
+        let age = person.yearOfDeath - person.yearOfBirth;
+
+        if (isNaN(age)) {
+            currentDate = new Date().getFullYear();
+            age = currentDate - person.yearOfBirth;
+        }
+
+        let ageObject = {
+            name,
+            age
+        }
+        ageArray.push(ageObject);
+    }
+    
+    const oldest = ageArray.sort((a,b) => {
+        const firstPerson = a.age;        const nextPerson = b.age;
+
+        return firstPerson > nextPerson ? -1 : 1;
+    })
+
+    let oldestName = oldest[0];
+    return oldestName;
 };
 
 // Do not edit below this line
