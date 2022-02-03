be77b781be69d67453688a97f8b93da02dc8fe9c
Qwarkeh <christine.f.stark@gmail.com>
2022-02-02 19:05:26 -0500

    passes all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num < 0) {
+        return "OOPS";
+    }
+    else {
+        let num1 = 0;
+        let num2 = 1;
+        var step = 0;
+
+        for (i = 1; i < num; i++) {
+            if (i < 1) {
+                step = i
+            }
+            else {
+                step = num1 + num2;
+                num1 = num2;
+                num2 = step;
+            }
+        }
+        return num2;
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// 1 1 2 3 5 8 13 21
